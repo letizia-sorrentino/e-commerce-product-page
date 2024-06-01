e711b82198696915b943608b9a19f23a0dc98c46
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2024-06-01 12:16:00 +0100

    updated styles

--- src/components/SliderDesktop.tsx
+++ src/components/SliderDesktop.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import iconNext from "../assets/icon-next.svg";
-import iconPrev from "../assets/icon-previous.svg";
-import iconClose from "../assets/icon-close.svg";
 import image1 from "../assets/image-product-1.jpg";
 import image2 from "../assets/image-product-2.jpg";
 import image3 from "../assets/image-product-3.jpg";
@@ -25,7 +22,7 @@ const SliderDesktop = () => {
   const currentIndex = useSelector(selectCurrentIndex);
   const isLightboxOpen = useSelector(selectIsOpen);
 
-  const open = () => {
+  const handleImageClick = () => {
     dispatch(openLightbox());
   };
 
@@ -52,90 +49,88 @@ const SliderDesktop = () => {
       document.body.classList.remove("overlay");
     }
   }, [isLightboxOpen]);
+
   return (
-    <>
-      <div className="sliderContainerDesktop">
-        {images.map((image, index) => (
+    <div className="sliderContainerDesktop">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className={`sliderImageDesktop ${
+            index === currentIndex ? "active" : "inactive"
+          }`}
+          src={image}
+          alt={`productImageDesktop ${index + 1}`}
+          onClick={handleImageClick}
+        />
+      ))}
+
+      {/* If isLightboxOpen is true, render the Lightbox component. Otherwise, render nothing. */}
+      {isLightboxOpen && (
+        <div className="lightboxContainer">
+          <button className="iconClose" onClick={close}>
+            <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+              <path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"></path>
+            </svg>
+          </button>
+
+          <button className="arrowPrev" onClick={prev}>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11 1 3 9l8 8"></path>
+            </svg>
+          </button>
+
+          <div className="lightboxImage">
+            {" "}
+            {images.map((image, index) => (
+              <img
+                key={index}
+                className={`sliderImageDesktop ${
+                  index === currentIndex ? "active" : "inactive"
+                }`}
+                src={image}
+                alt={`productImageDesktop ${index + 1}`}
+                onClick={next}
+              />
+            ))}
+          </div>
+          <button className="arrowNext" onClick={next}>
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path d="m2 1 8 8-8 8"></path>
+            </svg>
+          </button>
+
+          <div className="sliderDotsLightbox">
+            {images.map((_, index) => (
+              <img
+                key={index}
+                className={`sliderDotLightbox ${
+                  index === currentIndex ? "activeDot" : "inactiveDot"
+                }`}
+                src={images[index]}
+                // onClick={() => {
+                //   dispatch(setCurrentImage(images[index]));
+                //   dispatch(setCurrentIndex(index));
+                // }}
+                onClick={next}
+              ></img>
+            ))}
+          </div>
+        </div>
+      )}
+
+      <div className="sliderDotsDesktop">
+        {images.map((_, index) => (
           <img
             key={index}
-            className={`sliderImageDesktop ${
-              index === currentIndex ? "active" : "inactive"
+            className={`sliderDotDesktop ${
+              index === currentIndex ? "activeDot" : "inactiveDot"
             }`}
-            src={image}
-            alt={`productImageDesktop ${index + 1}`}
-            onClick={open}
-          />
+            src={images[index]}
+            onClick={next}
+          ></img>
         ))}
-
-        {/* If isLightboxOpen is true, render the Lightbox component. Otherwise, render nothing. */}
-        {isLightboxOpen && (
-          <div className="lightboxContainer">
-            <img
-              className="iconClose"
-              src={iconClose}
-              onClick={close}
-              alt="icon-close"
-            />
-            <img
-              className="arrowPrev"
-              src={iconPrev}
-              alt="icon-previous"
-              onClick={prev}
-            />
-            <div className="lightboxImage">
-              {" "}
-              {images.map((image, index) => (
-                <img
-                  key={index}
-                  className={`sliderImageDesktop ${
-                    index === currentIndex ? "active" : "inactive"
-                  }`}
-                  src={image}
-                  alt={`productImageDesktop ${index + 1}`}
-                  onClick={next}
-                />
-              ))}
-            </div>
-
-            <img
-              className="arrowNext"
-              src={iconNext}
-              alt="icon-next"
-              onClick={next}
-            />
-            <div className="sliderDotsLightbox">
-              {images.map((_, index) => (
-                <img
-                  key={index}
-                  className={`sliderDotLightbox ${
-                    index === currentIndex ? "activeDot" : "inactiveDot"
-                  }`}
-                  src={images[index]}
-                  // onClick={() => {
-                  //   dispatch(setCurrentImage(images[index]));
-                  //   dispatch(setCurrentIndex(index));
-                  // }}
-                  onClick={next}
-                ></img>
-              ))}
-            </div>
-          </div>
-        )}
-
-        <div className="sliderDotsDesktop">
-          {images.map((_, index) => (
-            <img
-              key={index}
-              className={`sliderDotDesktop ${
-                index === currentIndex ? "activeDot" : "inactiveDot"
-              }`}
-              src={images[index]}
-              onClick={next}
-            ></img>
-          ))}
-        </div>
-      </div>{" "}
-    </>
+      </div>
+    </div>
   );
 };
 export default SliderDesktop;
